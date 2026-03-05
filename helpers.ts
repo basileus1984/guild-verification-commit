38a213e545e120c1be8a79f7bc7562728c8e998b
basileus1984 <113292611+basileus1984@users.noreply.github.com>
2026-03-06 01:00:57 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:00:50.778Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T22:00:58.453Z
+Commit: Resolve null pointer exception in user service
