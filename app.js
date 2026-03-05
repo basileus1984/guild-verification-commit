62102d8f08ee58204e646bacb21c5c95c1f205f5
basileus1984 <113292611+basileus1984@users.noreply.github.com>
2026-03-06 00:59:39 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:59:32.490Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T21:59:40.357Z
+Commit: Resolve null pointer exception in user service
